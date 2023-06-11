fb7a66ff72a01fc2af290e4c7b8fe210b9789b0a
Gabriel Darlan <gabriel.darlan88@gmail.com>
2023-06-11 13:57:36 -0300

    deploy

---
+++ js/aula4.js
@@ -0,0 +1,9 @@
+const listaCores = document.querySelector("[lista]");
+
+const escondeTintas = () => {
+  listaCores.style.display = "none";
+};
+
+const mostraTintas = () => {
+  listaCores.style.display = "block";
+};
